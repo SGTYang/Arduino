3eeac61c8ed251d2cc35285e3c102a2022f7daf4
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-12-01 18:24:09 +0900

    Update sw_12-12.ino

--- sw_12-12.ino
+++ sw_12-12.ino
@@ -3,7 +3,6 @@ enum rot_sw_t {ROT_NONE, ROT_DN, ROT_UP, ROT_FALLING, ROT_RISING, ROT_CW, ROT_CC
 enum sw_style_t {SW_LONG, SW_SHORT, SW_NOTHING, SW_U_CW, SW_U_CCW, SW_D_CW, SW_D_CCW};
 long now_time;
 
-
 void setup() {
   init_sw();
   init_lcd();
